75b13cbde1bd16bce900432bbe133b25d1000923
Mohid2025 <mohidiftikhar2025@gmail.com>
2026-01-26 13:28:41 +0500

    day-2 lectures

--- day-2/task.py
+++ day-2/task.py
@@ -6,3 +6,28 @@
 # print(True)
 # print(False)
 
+# print(len("hello"))
+# print(type("hello"))
+# print(int("123") + int("456"))
+
+# print("Number of letters in your name: " + str(len(input("Enter your name: "))))
+
+# print("My age: " + str(24))
+# print(123 + 456)
+# print(7 - 3)
+# print(3 * 2)
+# print(8 / 2)
+# print(7 // 3)
+# print(2**3)
+# print(3 * 3 + 3 // 3 - 3)
+
+# print(8 / 3)
+# print(int(8 / 3))
+# print(round(8 / 3))
+# print(round(8 / 3, 2))
+
+# score = 0
+# height = 1.8
+# isWinning = True
+# print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
+
