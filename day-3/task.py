3b4af5dc8d3c9a037a1d4b4bba9d0275c679cf2b
Mohid2025 <mohidiftikhar2025@gmail.com>
2026-01-28 11:27:58 +0500

    day-3 lectures

---
+++ day-3/task.py
@@ -0,0 +1,94 @@
+# print("Welcome to the rollercoster")
+# height = int(input("What is your height in cm? "))
+
+# if height >= 120:
+#     print("You can ride the rollercoster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         print("Please pay $7.")
+#     else:
+#         print("Please pay $12.")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# num = int(input("Enter a number: "))
+
+# if(num % 2 == 0):
+#     print("This is an even number.")
+    
+# else:
+#     print("This is an odd number.")
+
+# print("Welcome to the rollercoster")
+# height = int(input("What is your height in cm? "))
+# total_bill = 0
+
+# if height >= 120:
+#     print("You can ride the rollercoster!")
+#     age = int(input("What is your age? "))
+
+#     if age < 12:
+#         total_bill = 5
+#         print("Please pay $5.")
+
+#     elif age <= 18:
+#         total_bill = 7
+#         print("Please pay $7.")
+
+#     else:
+#         total_bill = 12
+#         print("Please pay $12.")
+
+#     photo = input("Do you want a photo taken? Y or N. ")
+
+#     if photo == "y" or photo == "Y":
+#         total_bill += 3
+#         print(f"Your final bill is ${total_bill}.")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# print("Welcome to python pizza delivery!")
+
+# sm_pizza = 15
+# md_pizza = 20
+# lg_pizza = 25
+
+# sm_pep = 2
+# md_pep = lg_pep = 3
+# extra_cheese = 1
+
+# size = input("What size pizza do you want? S, M, or L: ")
+# add_pepperoni = input("Do you want pepperoni? Y or N: ")
+# cheese = input("Do you want extra cheese? Y or N: ")
+
+# bill = 0
+
+# if size == "S" or size == "s":
+#     bill += sm_pizza
+#     if add_pepperoni == "Y" or add_pepperoni == "y":
+#         bill += sm_pep
+#     if cheese == "Y" or cheese == "y":
+#         bill += extra_cheese
+
+# elif size == "M" or size == "m":
+#     bill += md_pizza
+#     if add_pepperoni == "Y" or add_pepperoni == "y":
+#         bill += md_pep
+#     if cheese == "Y" or cheese == "y":
+#         bill += extra_cheese
+
+# elif size == "L" or size == "l":
+#     bill += lg_pizza
+#     if add_pepperoni == "Y" or add_pepperoni == "y":
+#         bill += lg_pep
+#     if cheese == "Y" or cheese == "y":
+#         bill += extra_cheese
+
+# else:
+#     print("Invalid size selected.")
+
+# print(f"Your final bill is: ${bill}.")
+
+
