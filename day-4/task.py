ff95716bdf6e6215059028c4cf849be8e5b639e7
Mohid2025 <mohidiftikhar2025@gmail.com>
2026-02-02 12:05:22 +0500

    day-4 lectures

---
+++ day-4/task.py
@@ -0,0 +1,29 @@
+import random
+# import my_module
+
+# random_int = random.randint(1, 10)
+
+# print(f"Random integer between 1 and 10: {random_int}")
+# print(f"My favorite number is: {my_module.my_fav_num}")
+
+# random_float = random.random() * 1000
+# print(f"Random float between 0 and 1: {random_float}")
+
+# random_float = random.uniform(0, 10)
+# print(f"Random float between 0 and 10: {random_float}")
+
+# random_float = random.randint(0, 1)
+
+# if(random_float == 0):
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# friends = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+
+# leader = random.choice(friends)
+# print(f"The selected leader is: {leader}")
+
+# random_index = random.randint(0, len(friends) - 1)
+# print(f"The selected leader using random index is: {friends[random_index]}")
+
